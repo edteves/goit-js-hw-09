1fcded352b071d675e7916932e7dfb6fa6ca905b
edteves <edwinjrteves29@gmail.com>
2023-12-15 02:57:18 +0800

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let intervalId = null;
 
+stopBtn.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+body.addEventListener('click', onBtnClick);
+
+function onBtnClick(event) {
+  if (event.target.textContent === 'Start') {
+    intervalId = setInterval(() => {
+      const color = getRandomHexColor();
+      body.style.backgroundColor = color;
+    }, 1000);
+
+    stopBtn.disabled = false;
+    startBtn.disabled = true;
+  }
+
+  if (event.target.textContent === 'Stop') {
+    clearInterval(intervalId);
+
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+  }
+}
